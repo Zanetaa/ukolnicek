f9972421c3e867882da121e5a4463f79d8a0ada2
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-10-28 23:26:54 +0200

    click

--- index.js
+++ index.js
@@ -5,27 +5,50 @@ const Task = (props) => {
 
     let tickClass = '';
     if (done) {
-      tickClass = 'item__done--tick';
+        tickClass = 'item__done--tick';
     }
 
     return `
     <div class= "task__name">${name}</div>
     <div class= "task__due">${due}</div>
     <div class= "task__done">${tickClass}</div>
-    `; 
+    `;
 };
 
 
 const renderTasks = (props) => {
     const taskList = document.querySelector('.todo__tasks');
     taskList.innerHTML = props
-    .map((props) => Task(props))
-    .join('');
+        .map((props) => Task(props))
+        .join('');
 };
 
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
-.then((response) => response.json())
-.then((data) => renderTasks(data));
+    .then((response) => response.json())
+    .then((data) => renderTasks(data));
+
+
+const renderUndoneTasks = (props) => {
+    const taskList = document.querySelector('.todo__tasks');
+    taskList.innerHTML = props
+        .map((props) => Task(props))
+        .join('');
+};
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks?done=true')
+    .then((response) => response.json())
+    .then((data) => renderUndoneTasks(data))
+
+function myFunction() {
+    var checkBox = document.getElementById("checkbox-undone");
+    if (checkBox.checked == true) {
+        renderUndoneTasks;
+    } else {
+        renderTasks;
+    }
+}
+
+renderTasks;
 
 
 
