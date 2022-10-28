a663da9ee66d1df3eb4dfd7ccf6b2d23c2356cd5
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-10-28 22:39:21 +0200

    úpravy

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 console.log('funguju!');
 
 const Task = (props) => {
-    const { name, done } = props;
+    const { name, due, done } = props;
 
     let tickClass = '';
     if (done) {
-      tickClass = ' item__done--tick';
+      tickClass = 'item__done--tick';
     }
 
     return `
-    <div class= "todo__tasks">${name}</div>
-    <div class= "todo__tasks">${tickClass}</div>
-    `;
+    <div class= "task__name">${name}</div>
+    <div class= "task__due">${due}</div>
+    <div class= "task__done">${tickClass}</div>
+    `; 
 };
 
 
@@ -25,6 +26,8 @@ const renderTasks = (props) => {
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
 .then((response) => response.json())
 .then((data) => renderTasks(data));
+
+
 
 
 
