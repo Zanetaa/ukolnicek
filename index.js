015519a053c0f3801f110e98bd102212f2578274
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-10-28 21:48:49 +0200

    Task2

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 console.log('funguju!');
 
 const Task = (props) => {
-    const { name } = props;
+    const { name, done } = props;
+
+    let tickClass = '';
+    if (done) {
+      tickClass = ' item__done--tick';
+    }
 
     return `
     <div class= "todo__tasks">${name}</div>
+    <div class= "todo__tasks">${tickClass}</div>
     `;
-}
+};
+
+
 
 
